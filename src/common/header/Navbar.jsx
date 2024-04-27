debff7795d6f60eff5bc4cd6d37101bcc93af2fa
tokurovYasin <tokurovyasin3@gmail.com>
2024-04-27 22:57:48 +0600

    dd

--- src/common/header/Navbar.jsx
+++ src/common/header/Navbar.jsx
@@ -9,10 +9,10 @@ const Navbar = () => {
       <header className="header">
         <div className="container d_flex">
           <div className="catgrories d_flex">
-            <span class="fa-solid fa-border-all"></span>
+            {/* <span class="fa-solid fa-border-all"></span>
             <h4>
               Категория <i className="fa fa-chevron-down"></i>
-            </h4>
+            </h4> */}
           </div>
 
           <div className="navlink">
